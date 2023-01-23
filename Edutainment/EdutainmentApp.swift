ea4cf7c0156a73e0a00e6338d8ac40ec459ae7af
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-01-23 15:04:14 -0800

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Ikbal Demirdoven on 2023-01-23.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
